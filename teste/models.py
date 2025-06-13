009d189a8d357a0ae24855a0eef4ada05e3cc00a
Maria Clara Padilha Pires <mclarappires@gmail.com>
2025-06-13 16:13:23 -0300

    Delete teste directory

--- teste/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Ruas(models.Model):
-    title = models.CharField(max_lenght=50)
-    DESCRIPTION = models.TextField()
-    numdeRua = models.TextField()
